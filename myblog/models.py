2385a8dce38ba449d2a9c7d8a136a4d94dc683b8
bphariharan1301 <56637724+bphariharan1301@users.noreply.github.com>
2020-11-29 20:59:08 +0530

    Profile, Social Media Links, Article Detail Page Updated

--- myblog/models.py
+++ myblog/models.py
@@ -16,11 +16,25 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse('home')
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+    portfolio_url = models.CharField(max_length=100, null=True, blank=True)
+    fb_url = models.CharField(max_length=100, null=True, blank=True)
+    insta_url = models.CharField(max_length=100, null=True, blank=True)
+    twitter_url = models.CharField(max_length=100, null=True, blank=True)
+    linkedin_url = models.CharField(max_length=100, null=True, blank=True)
+    pinterest_url = models.CharField(max_length=100, null=True, blank=True)
+
+
+    def __str__(self):
+        return str(self.user)
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     # title_tag = models.CharField(max_length=100)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    header_image = models.ImageField(null=True, blank=True)
+    header_image = models.ImageField(null=True, blank=True, upload_to='images/')
     # category = models.ForeignKey(Category, on_delete=models.CASCADE)
     category = models.CharField(max_length=100)
     body = RichTextField(blank=True, null=True)
